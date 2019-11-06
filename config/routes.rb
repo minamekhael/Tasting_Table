3a9a86bc8840e0fefa877f27961fe7dbfa23a649
Mina Mekhael <mina.mekhael@ymail.com>
2019-11-05 19:02:34 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :categories
+  resources :recipes
+  # scope '/api' do
+    # get 'recipes/index'
+    # post 'recipes/create'
+    # get '/show/:id', to: 'recipes#show'
+    # delete '/destroy/:id', to: 'recipes#destroy'
+
+  # end
+end
